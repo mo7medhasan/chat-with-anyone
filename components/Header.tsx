49934bca1c6d36c3336e9fed9bd7f9537562a52f
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-10-22 23:32:36 +0300

    frist commit

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from './Logo'
+
+function Header() {
+  return (
+    <header>
+        <nav>
+           <Logo/>
+
+            {/* links */}
+        </nav>
+    </header>
+  )
+}
+
+export default Header
