0095e2b6d0a01ea1e9bd000401536e53efe01d8f
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-10-25 21:06:05 +0300

    update:header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import Link from 'next/link'
 import Logo from './Logo'
-import  DarkModeToggle from './DarkModeToggle'
+import DarkModeToggle from './DarkModeToggle'
 import UserButton from './UserButton'
-
-function Header() {
+import { authOptions } from '@/auth'
+import { getServerSession } from "next-auth"
+import {  MessagesSquareIcon } from 'lucide-react'
+async function Header() {
+  const session = await getServerSession(authOptions)
   return (
     <header className='sticky top-0 z-50 bg-white dark:bg-gray-900 '>
-        <nav className='flex flex-col sm:flex-row items-center p-5 pl-2 bg-white  dark:bg-gray-900  max-w-7xl mx-auto'>
-           <Logo/>
-          <div className='flex-1 flex items-center justify-end gap-x-6'>
-            {/* languageSelect */}
-            {/* session &&(
-              ...
-            )*/}
-            <DarkModeToggle/>
-            <UserButton />
-          </div>
-        </nav>
+      <nav className='flex flex-col sm:flex-row items-center p-5 pl-2 bg-white  dark:bg-gray-900  max-w-7xl mx-auto'>
+        <Logo />
+        <div className='flex-1 flex items-center justify-end gap-x-6'>
+          {/* languageSelect */}
+          {session ? (
+            <>
+
+              <Link href={'/chat'} prefetch={false}>
+                <MessagesSquareIcon className='text-black dark:text-white'
+                 /></Link>
+            </>
+          ):(
+            <Link href={'/pricing'} >Pricing</Link>
+          )}
+          <DarkModeToggle />
+          <UserButton />
+        </div>
+      </nav>
     </header>
   )
 }
