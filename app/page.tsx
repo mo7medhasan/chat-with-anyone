0cf75ca2e0464f9469ac7e5ada740affa13ae9ff
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-10-29 22:23:33 +0200

    feat:pricing page

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
             }} />
 
         </div>
-           </div>
+      </div>
     </main>
   )
 }
---
+++ components/PricingCards.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PricingCards({redirect}:{redirect:boolean}) {
+  return (
+    <div>PricingCards</div>
+  )
+}
+
+export default PricingCards
