d659cd5c7504f8d9ca98248dee76c08444dc0f33
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-02 19:02:58 +0200

    update:createCheckoutSession

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,24 +1,37 @@
 "use client"
 
+import { db } from "@/firebase"
+import { addDoc, collection } from "firebase/firestore"
 import { useSession } from "next-auth/react"
+import { useState } from "react"
 
 function CheckoutButton() {
-    const {data:session}=useSession()
- const createCheckoutSession=async()=>{
-        if(!session) return;
- }
-    return (
+  const { data: session } = useSession()
+  const [loading, setLoading] = useState(false)
+
+  const createCheckoutSession = async () => {
+    if (!session?.user.id) return;
+    setLoading(true)
+    const docRef = await addDoc(collection(db, "customers", session.user.id,
+      "checkout_sessions"), {
+      price: "price_1O0998KDjTc6lwiIL3eBoYQ",
+      success_url: window.location.origin,
+      cancel_url: window.location.origin,
+    })
+
+  }
+  return (
     <div className='flex flex-col space-y-2'>
 
-    <button onClick={()=>createCheckoutSession()} className="mt-8 block rounded-md font-semibold leading-6 bg-indigo-600 px-3.5 py-2 text-center text-sm
+      <button onClick={() => createCheckoutSession()} className="mt-8 block rounded-md font-semibold leading-6 bg-indigo-600 px-3.5 py-2 text-center text-sm
     text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
     focus-visible:outline-2 focus-visible: outline-offset-2 focus-visible:outline-indigo-600-
     cursor-pointer disabled: opacity-80 disabled:bg-indigo-600/50 disabled:text-white
     disabled:cursor-default">
-   
-    
-    Sign Up
-    </button></div>
+
+
+        Sign Up
+      </button></div>
   )
 }
 
