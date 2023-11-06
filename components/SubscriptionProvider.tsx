716a311ff4cae6261bf2840c531fd6bcaaa158aa
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-06 21:04:00 +0200

    update:SubscriptionProvider

--- components/SubscriptionProvider.tsx
+++ components/SubscriptionProvider.tsx
@@ -1,13 +1,23 @@
 "use client"
 
+import { subscriptionRef } from "@/lib/converters/Subscription"
+import { onSnapshot, snapshotEqual } from "firebase/firestore"
 import { useSession } from "next-auth/react"
 import { useEffect } from "react"
 
 function SubscriptionProvider() {
 const {data:session}=useSession()
 useEffect(() => {
- 
-}, [])
+ if(!session) return;
+return onSnapshot(subscriptionRef(session.user.id),(snapshot)=>{
+    if(snapshot.empty){
+        console.log('User has No subscription');
+        return; }
+        else{
+            console.log('User has  subscription');
+        }
+})
+}, [session])
 
 
   return (
