9fdde6d1d96b511e380edd49bb310e09cf1b3942
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-07 16:49:31 +0200

    feeat:ManageAccountButton and update :CheckoutButton

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -5,10 +5,16 @@ import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 import LoadingSpinner from "./LoadingSpinner";
+import { useSubscriptionStore } from "@/store/store";
+import ManageAccountButton from "./ManageAccountButton";
 
 function CheckoutButton() {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
+  const subscription = useSubscriptionStore((state) => state.subscription);
+  const isLoadingSubscription = subscription === undefined;
+  const isSubscribed = subscription?.status === "active" && subscription?.role === "pro";
+
 
   const createCheckoutSession = async () => {
     if (!session?.user.id) return;
@@ -38,16 +44,18 @@ function CheckoutButton() {
   };
   return (
     <div className="flex flex-col space-y-2">
-      <button
-        onClick={() => createCheckoutSession()}
-        className="mt-8 block w-full rounded-md font-semibold leading-6 bg-indigo-600 px-3.5 py-2 text-center text-sm
+      <div  className="mt-8 block w-full rounded-md font-semibold leading-6 bg-indigo-600 px-3.5 py-2 text-center text-sm
     text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
     focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600
     cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white
     disabled:cursor-default"
       >
-       {loading?(<LoadingSpinner/>):' Sign Up'}
-      </button>
+        {
+
+          isSubscribed ? (
+            <ManageAccountButton />
+          ) : isLoadingSubscription || loading ? (<LoadingSpinner />) : (<button onClick={() => createCheckoutSession()}> Sign Up</button>)}
+      </div>
     </div>
   );
 }
