04475b90cd3617420a9d53e124f9f431a6131bdb
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-08 17:26:03 +0200

    feat:toaster

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -1,9 +1,14 @@
 import { authOptions } from '@/auth';
+import { getDocs } from 'firebase/firestore';
 import { getServerSession } from 'next-auth';
 import React from 'react'
 
 async function ChatList() {
     const session = await getServerSession(authOptions);
+    // const chatSnapshot=await getDocs(
+    //     chatMembersCollectionGroupRef(session?.user.id!)
+    // )
+
   return (
     <div>ChatList</div>
   )
--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -3,13 +3,35 @@
 import { MessageSquarePlusIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { useRouter } from "next/navigation"
-
-function CreateChatButton() {
+import { useSession } from "next-auth/react"
+import {useState } from "react"
+import { useToast } from "./ui/use-toast"
+import { useSubscriptionStore } from "@/store/store"
+import LoadingSpinner from "./LoadingSpinner"
+function CreateChatButton({isLarge}:{isLarge?:boolean}) {
+  const {data:session}=useSession();
   const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const {toast}=useToast()
+  const setSubscription = useSubscriptionStore((state) => state.setSubscription)
+
   const createNewChat = async () => {
+    if (!session?.user.id) return;
+    setLoading(true);
+    toast({
+      title:"Create new chat...",
+      description:"Hold tight while we create your new chat...",
+      duration:3000
+    })
+    // const chatId=uuidv4()dzd
+
     router.push(`/chat/${'abc'}`)
   }
-
+if(isLarge)return(<div>
+  <Button variant={"default"} onClick={createNewChat}>
+    {loading?<LoadingSpinner/>:"create a New Chat"}
+  </Button>
+</div>)
   return (
     <Button variant={"ghost"}><MessageSquarePlusIcon /></Button>
   )
