d71b8a0e4d92b97628a4c4d8def22b44d23715eb
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-09 23:45:35 +0200

    feat : createNewChat

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -4,36 +4,68 @@ import { MessageSquarePlusIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { useRouter } from "next/navigation"
 import { useSession } from "next-auth/react"
-import {useState } from "react"
+import { useState } from "react"
 import { useToast } from "./ui/use-toast"
 import { useSubscriptionStore } from "@/store/store"
 import LoadingSpinner from "./LoadingSpinner"
-function CreateChatButton({isLarge}:{isLarge?:boolean}) {
-  const {data:session}=useSession();
+import { serverTimestamp, setDoc } from "firebase/firestore"
+
+import { v4 as uuidv4 } from 'uuid';
+import { addChatRef } from "@/lib/converters/ChatMembers"
+
+
+function CreateChatButton({ isLarge }: { isLarge?: boolean }) {
+  const { data: session } = useSession();
   const router = useRouter()
   const [loading, setLoading] = useState(false)
-  const {toast}=useToast()
-  const setSubscription = useSubscriptionStore((state) => state.setSubscription)
+  const { toast } = useToast()
 
   const createNewChat = async () => {
     if (!session?.user.id) return;
     setLoading(true);
     toast({
-      title:"Create new chat...",
-      description:"Hold tight while we create your new chat...",
-      duration:3000
+      title: "Create new chat...",
+      description: "Hold tight while we create your new chat...",
+      duration: 3000
     })
-    // const chatId=uuidv4()dzd
+    const chatId = uuidv4();
 
-    router.push(`/chat/${'abc'}`)
-  }
-if(isLarge)return(<div>
-  <Button variant={"default"} onClick={createNewChat}>
-    {loading?<LoadingSpinner/>:"create a New Chat"}
-  </Button>
-</div>)
+
+    await setDoc(
+      addChatRef(chatId, session.user.id), {
+      userId: session.user.id!,
+      email: session.user.email!,
+      timestamp: serverTimestamp(),
+      isAdmin: true,
+      chatId: chatId,
+      image: session.user.image || " "
+    }).then(() => {
+      toast({
+        title: "Success",
+        description: "Your chat has been created!",
+        className:"bg-green-600 text-white",
+        duration: 2000
+      });
+      router.push(`/chat/${chatId}`)
+    }).catch((error) => {
+      console.error(error)
+      toast({
+        title: "Error",
+        description: "There was an error creating your chat!",
+        variant: "destructive"
+      });
+    }).finally(() => {
+      setLoading(false)
+    })
+
+  };
+  if (isLarge) return (<div>
+    <Button variant={"default"} onClick={createNewChat}>
+      {loading ? <LoadingSpinner /> : "create a New Chat"}
+    </Button>
+  </div>)
   return (
-    <Button variant={"ghost"}><MessageSquarePlusIcon /></Button>
+    <Button variant={"ghost"} onClick={createNewChat}><MessageSquarePlusIcon /></Button>
   )
 }
 
--- lib/converters/ChatMembers.ts
+++ lib/converters/ChatMembers.ts
@@ -7,28 +7,28 @@ import {
   collection,
   collectionGroup,
   doc,
-  query,where,
+  query,
+  where,
 } from "firebase/firestore";
 
 export interface ChatMembers {
-  userId:string;
-  email:string;
-  timestamp:string;
-  isAdmin:string;
-  chatId:string;
-  image:string;
-
+  userId: string;
+  email: string;
+  timestamp: string;
+  isAdmin: boolean;
+  chatId: string;
+  image: string;
 }
 
 const chatMembersConverter: FirestoreDataConverter<ChatMembers> = {
   toFirestore: function (member: ChatMembers): DocumentData {
     return {
-      userId:member.userId,
-      email:member.email,
-      timestamp:member.timestamp,
-      isAdmin:member.isAdmin,
-      chatId:member.chatId,
-      image:member.image
+      userId: member.userId,
+      email: member.email,
+      timestamp: member.timestamp,
+      isAdmin: member.isAdmin,
+      chatId: member.chatId,
+      image: member.image,
     };
   },
   fromFirestore(
@@ -37,36 +37,34 @@ const chatMembersConverter: FirestoreDataConverter<ChatMembers> = {
   ): ChatMembers {
     const data = snapshot.data(options);
     return {
-
-      userId:snapshot.id,
-      email:data.email,
-      timestamp:data.timestamp,
-      isAdmin:data.isAdmin,
-      chatId:data.chatId,
-      image:data.image
+      userId: snapshot.id,
+      email: data.email,
+      timestamp: data.timestamp,
+      isAdmin: data.isAdmin,
+      chatId: data.chatId,
+      image: data.image,
     };
   },
 };
 
-export const addChatRef=(chatId:string,userId:string)=>collection(db,"chats",chatId,"members",userId).withConverter(
-  chatMembersConverter
-);
+export const addChatRef = (chatId: string, userId: string) =>
+  doc(db, "chats", chatId, "members", userId).withConverter(
+    chatMembersConverter
+  );
 
-export const chatMembersRef=(chatId:string)=>collection(db,"chats",chatId,"members").withConverter(
-  chatMembersConverter
-);
+export const chatMembersRef = (chatId: string) =>
+  collection(db, "chats", chatId, "members").withConverter(
+    chatMembersConverter
+  );
 
+export const chatMemberAdminRef = (chatId: string) =>
+  query(
+    collection(db, "chats", chatId, "members"),
+    where("isAdmin", "==", true)
+  ).withConverter(chatMembersConverter);
 
-
-export const chatMemberAdminRef=(chatId:string)=>
-query(
-collection(db,"chats",chatId,"members"),where("isAdmin","==",true)).withConverter(
-  chatMembersConverter
-);
-
-
-export const chatMembersCollectGroupRef=(userId:string)=>
-query(
-collectionGroup(db,"members"),where("userId","==",userId)).withConverter(
-  chatMembersConverter
-);
+export const chatMembersCollectGroupRef = (userId: string) =>
+  query(
+    collectionGroup(db, "members"),
+    where("userId", "==", userId)
+  ).withConverter(chatMembersConverter);
