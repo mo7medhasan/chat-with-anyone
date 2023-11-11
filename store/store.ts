ff595cf06ab3b1acfd4970e337ddcef75fcbafa2
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-11 21:21:39 +0200

    feat :Languages & style : ChatListRow

--- store/store.ts
+++ store/store.ts
@@ -1,37 +1,65 @@
-import  {create} from 'zustand';
-import { Subscription } from '@/types/Subscription';
+import { create } from "zustand";
+import { Subscription } from "@/types/Subscription";
 
-export type LanguagesSupported=
-|"en"
-|"de"
-|"fr"
-|"es"
-|"hi"
-|"ja"
-|"la"
-|"ru"
-|"zh"
-|"ar"
-;
-export const LanguagesSupportedMap:Record<LanguagesSupported,string>={
-    en:"English",
-    de:"German",
-    fr:"French",
-    es:"Spanish",
-    hi:"Hindi",
-    ja:"Japanese",
-    la:"Latin",
-    ru:"Russian",
-    zh:"Mandarin",
-    ar:"Arabic", 
+export type LanguagesSupported =
+  | "en"
+  | "ar"
+  | "de"
+  | "fr"
+  | "es"
+  | "hi"
+  | "ja"
+  | "la"
+  | "ru"
+  | "zh";
+export const LanguagesSupportedMap: Record<LanguagesSupported, string> = {
+  en: "English",
+  ar: "Arabic",
+  de: "German",
+  fr: "French",
+  es: "Spanish",
+  hi: "Hindi",
+  ja: "Japanese",
+  la: "Latin",
+  ru: "Russian",
+  zh: "Mandarin",
 };
 
-interface SubscriptionState{
-    subscription:Subscription|null|undefined;
-    setSubscription:(subscription:Subscription|null)=>void;
+const LANGUAGES_IN_FREE = 2;
+
+interface LanguageState {
+  language: LanguagesSupported;
+  setLanguage: (language: LanguagesSupported) => void;
+  getLanguages: (isPro: boolean) => LanguagesSupported[];
+  getNotSupportedLanguages: (isPro: boolean) => LanguagesSupported[];
 }
 
-export const useSubscriptionStore=create<SubscriptionState>((set)=>({
-    subscription:undefined,
-    setSubscription:(subscription:Subscription|null)=>set({subscription})
-}))
+export const useLanguageStore = create<LanguageState>()((set, get) => ({
+  language: "en",
+  setLanguage: (language: LanguagesSupported) => set({ language }),
+  getLanguages: (isPro: boolean) => {
+    if (isPro)
+      return Object.keys(LanguagesSupportedMap) as LanguagesSupported[];
+
+    return Object.keys(LanguagesSupportedMap).slice(
+      0,
+      LANGUAGES_IN_FREE
+    ) as LanguagesSupported[];
+  },
+  getNotSupportedLanguages: (isPro: boolean) => {
+    if (isPro) return [];
+    return Object.keys(LanguagesSupportedMap).slice(
+      LANGUAGES_IN_FREE
+    ) as LanguagesSupported[];
+  },
+}));
+
+interface SubscriptionState {
+  subscription: Subscription | null | undefined;
+  setSubscription: (subscription: Subscription | null) => void;
+}
+
+export const useSubscriptionStore = create<SubscriptionState>((set) => ({
+  subscription: undefined,
+  setSubscription: (subscription: Subscription | null) => set({ subscription }),
+}));
