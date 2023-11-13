53f862c80937a467f6abf8c1ca62df24a4e8248f
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-13 14:22:15 +0200

    update : ChatMessages

--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -1,17 +1,87 @@
-import { Message } from "@/lib/converters/Message"
+"use client";
+import { Message, sortedMessageRef } from "@/lib/converters/Message";
+import { useLanguageStore } from "@/store/store";
+import { MessageCircleIcon } from "lucide-react";
 import { Session } from "next-auth";
+import { createRef, useEffect } from "react";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import LoadingSpinner from "./LoadingSpinner";
+import UserAvatar from "./UserAvatar";
 
+function ChatMessages({
+  chatId,
+  session,
+  initialMessages,
+}: {
+  chatId: string;
+  session: Session | null;
+  initialMessages: Message[];
+}) {
+  const language = useLanguageStore((state) => state.language);
+  const messagesEndRef = createRef<HTMLDivElement>();
 
-function ChatMessages({chatId,
-    session,
-    initialMessages
-    }:{chatId:string;
-        session:Message[] ;
-        initialMessages:Session|null;
-        }) {
+  const [messages, loading, error] = useCollectionData<Message>(
+    sortedMessageRef(chatId),
+    {
+      initialValue: initialMessages,
+    }
+  );
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages, messagesEndRef]);
+
   return (
-    <div>ChatMessages</div>
-  )
+    <div className="p-5">
+      {!loading && messages?.length === 0 && (
+        <div
+          className="flex flex-col justify-center items-center text-center p-20 rounded-xl 
+            space-y-2 bg-indigo-400 text-white font-extralight"
+        >
+          <MessageCircleIcon className="h-10 w-10" />
+          <h2>
+            <span className="font-bold">Invite a friend</span>&{" "}
+            <span className="font-bold">
+              send your first message in ANY language
+            </span>{" "}
+            below to get started!
+          </h2>
+          <p>the AI will auto-detect & translate it all for you</p>
+        </div>
+      )}
+      {messages?.map((message) => {
+        const isSender = message.user.id === session?.user.id;
+        return (
+          <div key={message.id} className="flex my-2 items-end">
+            <div
+              className={`flex flex-col relative space-y-2 p-4 w-fit line-clamp-1 mx-2 rounded-lg 
+${
+  isSender
+    ? "ml-auto bg-violet-600 text-white rounded-br-none "
+    : "bg-gray-100 dark:text-gray-100 dark:bg-slate-700 rounded-bl-none"
+}`}
+            >
+              <p
+                className={`text-xs italic font-extralight line-clamp-1 ${
+                  isSender ? "text-right" : "text-left"
+                }`}
+              >
+                {message.user.name.split(" ")[0]}
+              </p>
+              <div className="flex space-x-2">
+                <p>
+                    {message.translated?.[language]||message.input}
+                </p>
+                {!message.translated && <LoadingSpinner/>}
+              </div>
+            </div>
+            <UserAvatar name={message.user.name}
+            image={message.user.image}
+            className={`${!isSender&&"-order-1"}`} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default ChatMessages
+export default ChatMessages;
