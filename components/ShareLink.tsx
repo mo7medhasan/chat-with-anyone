872e2b8c3472bff60ee7fa1ad6cea71744f02347
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-18 17:30:12 +0200

    feat:ShareLink

--- components/ShareLink.tsx
+++ components/ShareLink.tsx
@@ -1,21 +1,23 @@
-"use client"
+"use client";
 import React, { Dispatch, SetStateAction } from "react";
 import { Copy } from "lucide-react";
 import {
-    Dialog,DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,DialogFooter,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog";
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
+import { Label } from "./ui/label";
+import { Input } from "@/components/ui/input";
 
-  import { Input } from "@/components/ui/input";
-
-  import { useToast } from "./ui/use-toast";
-  import { ToastAction } from "@radix-ui/react-toast";
-
+import { useToast } from "./ui/use-toast";
+import { ToastAction } from "@radix-ui/react-toast";
+import { Button } from "./ui/button";
 
 function ShareLink({
   isOpen,
@@ -26,7 +28,75 @@ function ShareLink({
   chatId: string;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) {
-  return <div>ShareLink</div>;
+  const { toast } = useToast();
+  const host = window.location.host;
+
+  const linkToChat =
+    process.env.NODE_ENV === "development"
+      ? `http://${host}/chat/${chatId}`
+      : `https://${host}/chat/${chatId}`;
+
+  async function copyToClipboard() {
+    try {
+      await navigator.clipboard.writeText(linkToChat);
+      console.log("Text copied to clipboard");
+      toast({
+        title: "Copied Successfully",
+        description:
+          "Share this to the person you want to chat with! (NOTE: They must be added to the Chat to access it!)",
+        className: "bg—green-600 text-white",
+      });
+    } catch (error) {
+      console.error("Failed to copy text: ", error);
+    }
+  }
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(open) => setIsOpen(open)}
+      defaultOpen={isOpen}
+    >
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Copy className="mr-2" />
+          share Link
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogDescription>
+            Any user who has been{" "}
+            <span className="text-indigo-600 font-bold">granted access</span>
+            can use this link
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex space-x-2 items-center">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              link
+            </Label>
+            <Input id="link" defaultValue={linkToChat} readOnly />
+          </div>
+          <Button
+            type="submit"
+            onClick={() => copyToClipboard()}
+            className="px-3"
+          >
+            <span className="sr-only">Copy</span>
+            <Copy className="h-4 w-4" />
+          </Button>
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant={"secondary"}>
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default ShareLink;
