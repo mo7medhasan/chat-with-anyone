258030afbb9ba9a6075e4a784093d678c33cda9e
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-19 20:47:17 +0200

    feat:DeleteChatButton

---
+++ components/DeleteChatButton.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { useState } from "react"
+import { Button } from "./ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { useToast } from "./ui/use-toast";
+import { ToastAction } from "@radix-ui/react-toast";
+import useAdminId from "@/hooks/useAdminId";
+
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+function DeleteChatButton({ chatId }: { chatId: string }) {
+    const { data: session } = useSession();
+    const [open, setOpen] = useState(false);
+    const { toast } = useToast()
+    const router = useRouter();
+    const adminId = useAdminId({ chatId });
+
+const  handleDelete=async()=>{
+toast({
+    title: "Deleting chat" ,
+description: "Please wait while we delete the chat..." ,
+})
+console.log("Delete :: ",chatId)
+}
+    
+
+    return adminId === session?.user.id && (
+
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button variant={"destructive"}>
+                    Delete chat
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                    <DialogTitle>
+                        Are you sure?
+                    </DialogTitle>
+                    <DialogDescription>
+                        this will delete the chat for all users.
+                    </DialogDescription>
+                </DialogHeader>
+
+
+                <div className="grid grid-cols-2 space-x-2 " >
+
+                    <Button variant={"destructive"}
+                    onClick={handleDelete}
+                    >
+                        Delete
+                    </Button>
+                    <Button variant={"outline"}
+                        onClick={() => setOpen(false)}>
+                        Cancel
+                    </Button>
+                </div>
+            </DialogContent>
+        </Dialog>
+
+
+
+    )
+}
+
+export default DeleteChatButton
