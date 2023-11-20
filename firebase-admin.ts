f4d0fc934bfea7269f58a04ef8fa6cec965b5442
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-11-21 01:06:42 +0200

    update

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -7,7 +7,7 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY? JSON.parse(process.env.FIREBASE_PRIVATE_KEY): undefined,
     }),
   });
 }
