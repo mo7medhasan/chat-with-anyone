6fc2cebe5d4e348c88842a64a36bf7be8a316a8b
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-12-23 17:30:18 +0200

    fix next-auth/middleware

--- middleware.ts
+++ middleware.ts
@@ -2,5 +2,5 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth;
 
 export const config={
-    matcher:["/chat","/chat/:id*","/register"]
+    matcher:["/chat/:id*","/register"]
 }
