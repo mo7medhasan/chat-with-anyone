c249fd2a40a8058b488f63605fee1177f51204a2
mo7medhasan <87263653+mo7medhasan@users.noreply.github.com>
2023-12-23 17:40:23 +0200

    test next-auth/middleware

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -16,7 +16,7 @@ function LoadingSpinner() {
           cy="12"
           r="10"
           stroke="currentColor"
-          stroke-width="4"
+          strokeWidth="4"
         ></circle>
         <path
           className="opacity-75"
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 import { withAuth } from "next-auth/middleware";
-export default withAuth;
-
+export default withAuth(
+    // `withAuth` augments your `Request` with the user's token.
+    function middleware(req) {
+      console.log(req.nextauth.token)
+    },
+    
+  )
+  
 export const config={
-    matcher:["/chat/:id*","/register"]
+    matcher:["/chat","/chat/:id*","/register"]
 }
